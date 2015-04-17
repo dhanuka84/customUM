f0c24b7dd1f39ca7884cb462b6286af0be3d335c
Dhannuka Priyanath Ranasinghe <dhanuka.priyanath@gmail.com>
2015-04-18 00:07:54 +0530

    remove comments

--- src/main/java/com/wso2/custom/usermgt/CustomUserStoreManager.java
+++ src/main/java/com/wso2/custom/usermgt/CustomUserStoreManager.java
@@ -9,7 +9,7 @@ import org.wso2.carbon.user.core.UserStoreException;
 import org.wso2.carbon.user.core.claim.ClaimManager;
 import org.wso2.carbon.user.core.jdbc.JDBCUserStoreManager;
 import org.wso2.carbon.user.core.profile.ProfileConfigurationManager;
- 
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -21,8 +21,6 @@ import java.util.Map;
  
 public class CustomUserStoreManager extends JDBCUserStoreManager {
    private static Log log = LogFactory.getLog(CustomUserStoreManager.class);
-   // This instance is used to generate the hash values
-   /*private static StrongPasswordEncryptor passwordEncryptor = new StrongPasswordEncryptor();*/
  
    // You must implement at least one constructor
    public CustomUserStoreManager(RealmConfiguration realmConfig, Map<String, Object> properties, ClaimManager
@@ -53,26 +51,9 @@ public class CustomUserStoreManager extends JDBCUserStoreManager {
  
                prepStmt = dbConnection.prepareStatement(sql);
                prepStmt.setString(1, userName);
-              /* // check whether tenant id is used
-               if (sql.contains("")) {
-                   prepStmt.setInt(2, this.tenantId);
-               }
- */
                rs = prepStmt.executeQuery();
                if (rs.next()) {
                    String storedPassword = rs.getString(2);
- 
-                   // check whether password is expired or not
-                 /*  boolean requireChange = rs.getBoolean(5);
-                   Timestamp changedTime = rs.getTimestamp(6);
-                   GregorianCalendar gc = new GregorianCalendar();
-                   gc.add(GregorianCalendar.HOUR, -24);
-                   Date date = gc.getTime();
-                   if (!(requireChange && changedTime.before(date))) {
-                       // compare the given password with stored password using jasypt
-                       //isAuthenticated = passwordEncryptor.checkPassword(candidatePassword, storedPassword);
-                	   isAuthenticated = candidatePassword.equalsIgnoreCase(storedPassword);
-                   }*/
                    isAuthenticated = candidatePassword.equalsIgnoreCase(storedPassword);
                }
                log.info(userName + " is authenticated? " + isAuthenticated);
@@ -84,15 +65,13 @@ public class CustomUserStoreManager extends JDBCUserStoreManager {
        return isAuthenticated;
    }
  
-   /*@Override
-   protected String preparePassword(String password, String saltValue) throws UserStoreException {
-       if (password != null) {
-           // ignore saltValue for the time being
-           log.info("Generating hash value using jasypt...");
-           return passwordEncryptor.encryptPassword(password);
-       } else {
-           log.error("Password cannot be null");
-           throw new UserStoreException("Authentication Failure");
-       }
-   }*/
+   @Override
+	public void addUser(String userName, Object credential, String[] roleList,
+			Map<String, String> claims, String profileName)
+			throws UserStoreException {
+		super.addUser(userName, credential, roleList, claims, profileName);
+		
+	}
+   
+   
 }
